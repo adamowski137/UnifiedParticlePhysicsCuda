edd2b9078a28036f55e197de750667df1bbec1dd
ingeniousname <miloszwozny5@gmail.com>
2024-02-19 14:53:40 +0100

    CUDA setup

---
+++ UnifiedParticlePhysicsCuda/UnifiedParticlePhysicsCuda.cpp
@@ -0,0 +1,18 @@
+﻿// UnifiedParticlePhysicsCuda.cpp : Defines the entry point for the application.
+//
+
+#include "UnifiedParticlePhysicsCuda.h"
+
+
+using namespace std;
+
+int main()
+{
+	int A[4] = { 1, 1, 1, 1 };
+	int B[4] = { 1, 2, 3, 4 };
+	addWithCuda(A, A, B, 4);
+	for (int i = 0; i < 4; i++)
+		std::cout << A[i] << "\n";
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ UnifiedParticlePhysicsCuda/UnifiedParticlePhysicsCuda.h
@@ -0,0 +1,12 @@
+﻿// UnifiedParticlePhysicsCuda.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <cuda_runtime.h>
+
+__global__ void addKernel(int* c, const int* a, const int* b);
+cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size);
+
+// TODO: Reference additional headers your program requires here.
